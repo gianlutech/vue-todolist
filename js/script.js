d0ccba14225051f601aefe8fcdaf2fee37edb94d
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-29 19:30:28 +0100

    add player

--- js/script.js
+++ js/script.js
@@ -4,18 +4,35 @@ console.log('Vue OK', Vue);
 const root = new Vue({
     el: '#root',
     data: {
+        newPlayer: '',
         players: [
-            { text: 'Dusan Vlahovic', done: '', },
-            { text: 'Ibra', done: '', },
-            { text: 'messi', done: '', },
-            { text: 'Ronaldo', done: '', },
-            { text: 'Dusan Vlahovic', done: '', },
+            { text: 'Dusan Vlahovic', done: true, },
+            { text: 'Ibra', done: false, },
+            { text: 'Leo Messi', done: false, },
+            { text: 'Cristiano Ronaldo', done: true, },
+            { text: 'Manuel Locatelli', done: true, },
         ]
     },
     methods: {
         deletTask(index) {
             this.players.splice(index, 1);
-            console.log(index)
+
+        },
+
+        addPlayer() {
+            const newPlayer = this.newPlayer.trim();
+            if (newPlayer) {
+                const newPlayerObject = {
+                    text: newPlayer,
+                    done: false,
+                }
+                this.players.push(newPlayerObject);
+            }
+            this.newPlayer = '';
+        },
+
+        toggleTaskDone() {
+
         }
     }
 });
